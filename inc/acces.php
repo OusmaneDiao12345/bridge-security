9a3150a6b5a875f3e82bee0e3cb0f063b70da367
Fil <fil@rezo.net>
2006-02-28 23:54:14 +0000

    déplacement d'une belle quantité de inc_*

---
+++ inc/acces.php
@@ -0,0 +1,200 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2006                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+
+//
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+function creer_pass_aleatoire($longueur = 8, $sel = "") {
+	$seed = (double) (microtime() + 1) * time();
+	mt_srand($seed);
+	srand($seed);
+	$s = '';
+	$pass = '';
+	for ($i = 0; $i < $longueur; $i++) {
+		if (!$s) {
+			$s = mt_rand();
+			if (!$s) $s = rand();
+			$s = substr(md5(uniqid($s).$sel), 0, 16);
+		}
+		$r = unpack("Cr", pack("H2", $s.$s));
+		$x = $r['r'] & 63;
+		if ($x < 10) $x = chr($x + 48);
+		else if ($x < 36) $x = chr($x + 55);
+		else if ($x < 62) $x = chr($x + 61);
+		else if ($x == 63) $x = '/';
+		else $x = '.';
+		$pass .= $x;
+		$s = substr($s, 2);
+	}
+	$pass = ereg_replace("[./]", "a", $pass);
+	$pass = ereg_replace("[I1l]", "L", $pass);
+	$pass = ereg_replace("[0O]", "o", $pass);
+	return $pass;
+}
+
+
+//
+// low-security : un ensemble de fonctions pour gerer de l'identification
+// faible via les URLs (suivi RSS, iCal...)
+//
+function low_sec($id_auteur) {
+	// Pas d'id_auteur : low_sec
+	if (!$id_auteur = intval($id_auteur)) {
+		if (!$low_sec = $GLOBALS['meta']['low_sec']) {
+			include_ecrire('inc_meta');
+			ecrire_meta('low_sec', $low_sec = creer_pass_aleatoire());
+			ecrire_metas();
+		}
+	}
+	else {
+		$query = "SELECT * FROM spip_auteurs WHERE id_auteur = $id_auteur";
+		$result = spip_query($query);
+
+		if ($row = spip_fetch_array($result)) {
+			$low_sec = $row["low_sec"];
+			if (!$low_sec) {
+				$low_sec = creer_pass_aleatoire();
+				spip_query("UPDATE spip_auteurs SET low_sec = '$low_sec' WHERE id_auteur = $id_auteur");
+			}
+		}
+	}
+	return $low_sec;
+}
+
+function afficher_low_sec ($id_auteur, $action='') {
+	return substr(md5($action.low_sec($id_auteur)),0,8);
+}
+
+function verifier_low_sec ($id_auteur, $cle, $action='') {
+	return ($cle == afficher_low_sec($id_auteur, $action));
+}
+
+function effacer_low_sec($id_auteur) {
+	if (!$id_auteur = intval($id_auteur)) return; // jamais trop prudent ;)
+	spip_query("UPDATE spip_auteurs SET low_sec = '' WHERE id_auteur = $id_auteur");
+}
+
+function initialiser_sel() {
+	global $htsalt;
+
+	$htsalt = '$1$'.creer_pass_aleatoire();
+}
+
+
+function ecrire_logins($fichier, $tableau_logins) {
+	reset($tableau_logins);
+
+	while(list($login, $htpass) = each($tableau_logins)) {
+		if ($login && $htpass) {
+			fputs($fichier, "$login:$htpass\n");
+		}
+	}
+}
+
+
+function ecrire_acces() {
+	$htaccess = _DIR_RESTREINT . _ACCESS_FILE_NAME;
+	$htpasswd = _DIR_SESSIONS . _AUTH_USER_FILE;
+
+	// si .htaccess existe, outrepasser spip_meta
+	if (($GLOBALS['meta']['creer_htpasswd'] == 'non') AND !@file_exists($htaccess)) {
+		@unlink($htpasswd);
+		@unlink($htpasswd."-admin");
+		return;
+	}
+
+	# remarque : ici on laisse passer les "nouveau" de maniere a leur permettre
+	# de devenir "1comite" le cas echeant (auth http)... a nettoyer
+	$query = "SELECT login, htpass FROM spip_auteurs WHERE statut != '5poubelle' AND statut!='6forum'";
+	$result = spip_query_db($query);	// attention, il faut au prealable se connecter a la base (necessaire car utilise par install)
+	$logins = array();
+	while($row = spip_fetch_array($result)) $logins[$row['login']] = $row['htpass'];
+
+	$fichier = @fopen($htpasswd, "w");
+	if ($fichier) {
+		ecrire_logins($fichier, $logins);
+		fclose($fichier);
+	} else {
+	  redirige_par_entete(generer_url_action('test_dirs', '', true));
+	}
+
+	$query = "SELECT login, htpass FROM spip_auteurs WHERE statut = '0minirezo'";
+	$result = spip_query_db($query);
+
+	$logins = array();
+	while($row = spip_fetch_array($result)) $logins[$row['login']] = $row['htpass'];
+
+	$fichier = fopen("$htpasswd-admin", "w");
+	ecrire_logins($fichier, $logins);
+	fclose($fichier);
+}
+
+
+function generer_htpass($pass) {
+	global $htsalt;
+	if (function_exists('crypt'))
+		return crypt($pass, $htsalt);
+}
+
+//
+// Verifier la presence des .htaccess
+//
+function verifier_htaccess($rep) {
+	$htaccess = "$rep/" . _ACCESS_FILE_NAME;
+	if ((!@file_exists($htaccess)) AND 
+	    !defined('_ECRIRE_INSTALL') AND !defined('_TEST_DIRS')) {
+		spip_log("demande de creation de $htaccess");
+		if ($_SERVER['SERVER_ADMIN'] != 'www@nexenservices.com'){
+			if (!$f = fopen($htaccess, "w"))
+				echo "<b>" .
+				  "ECHEC DE LA CREATION DE $htaccess" . # ne pas traduire
+				  "</b>";
+			else
+			  {
+				fputs($f, "deny from all\n");
+				fclose($f);
+			  }
+		} else {
+			echo "<font color=\"#FF0000\">IMPORTANT : </font>";
+			echo "Votre h&eacute;bergeur est Nexen Services.<br />";
+			echo "La protection du r&eacute;pertoire <i>$rep/</i> doit se faire
+			par l'interm&eacute;diaire de ";
+			echo "<a href=\"http://www.nexenservices.com/webmestres/htlocal.php\"
+			target=\"_blank\">l'espace webmestres</a>.";
+			echo "Veuillez cr&eacute;er manuellement la protection pour
+			ce r&eacute;pertoire (un couple login/mot de passe est
+			n&eacute;cessaire).<br />";
+		}
+	}
+}
+
+function gerer_htaccess() {
+	$mode = $GLOBALS['meta']['creer_htaccess'];
+	$r = spip_query("SELECT extension FROM spip_types_documents");
+	while ($e = spip_fetch_array($r)) {
+		if (is_dir($dir = _DIR_DOC . $e['extension'])) {
+			if ($mode == 'oui')
+				verifier_htaccess($dir);
+			else @unlink("$dir/" . _ACCESS_FILE_NAME);
+		}
+	}
+	return $mode;
+}
+
+// En profiter pour verifier la securite de ecrire/data/
+verifier_htaccess(_DIR_SESSIONS);
+
+initialiser_sel();
+
+?>
---
+++ inc/admin.php
@@ -0,0 +1,79 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2006                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function fichier_admin($action) {
+	global $connect_login;
+	return "admin_".substr(md5($action.(time() & ~2047).$connect_login), 0, 10);
+}
+
+function debut_admin($form, $action, $commentaire='') {
+	global $clean_link;
+	global $connect_statut;
+
+	if ((!$action) || ($connect_statut != "0minirezo")) {
+		include_ecrire ("inc_minipres");
+		minipres(_T('info_acces_refuse'));
+	}
+	$fichier = fichier_admin($action);
+	if (@file_exists(_DIR_SESSIONS . $fichier)) {
+		spip_log ("Action admin: $action");
+		return true;
+	}
+	if ($commentaire) {
+		include_ecrire ("inc_texte");
+		$commentaire = ("\n<p>".propre($commentaire)."</p>\n");
+	}
+	include_ecrire ("inc_minipres");
+	minipres(_T('info_action', array('action' => $action)),
+		  $commentaire
+		. $form
+		. "\n<p><b>"._T('info_authentification_ftp')."</b>"
+		. aide("ftp_auth")
+		. "\n<p>"
+		. _T('info_creer_repertoire')
+		. "\n<p align='center'>\n<INPUT TYPE='text' NAME='fichier' CLASS='fondl' VALUE=\"".
+		 $fichier
+		. "\" size='30'>"
+		. "\n<p>"
+		. _T('info_creer_repertoire_2', array('repertoire' => _DIR_SESSIONS))
+		. "\n<p align='right'><INPUT TYPE='submit' VALUE='"
+		. _T('bouton_recharger_page')
+		. "' CLASS='fondo'>"
+		. "</form>");
+}
+
+function fin_admin($action) {
+	$fichier = fichier_admin($action);
+	@unlink(_DIR_SESSIONS . $fichier);
+	@rmdir(_DIR_SESSIONS . $fichier);
+}
+
+
+function demande_maj_version() {
+	include_ecrire("inc_presentation");
+	debut_page();
+	echo "<blockquote><blockquote><h4><font color='red'>",
+	_T('info_message_technique'),
+	"</font><br> ",
+	_T('info_procedure_maj_version'),
+	"</h4>",
+	_T('info_administrateur_site_01'),
+	" <a href='" . generer_url_ecrire("upgrade","reinstall=non") . "'>",
+	_T('info_administrateur_site_02'),
+	"</a></blockquote></blockquote><p>";
+	fin_page();
+	exit;
+}
+
+?>
